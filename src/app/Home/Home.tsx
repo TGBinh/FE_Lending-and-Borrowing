5286dcb634b97d2f0af00aaef3fff5a60181214e
TGBinh <truonggiabinh1231@gmail.com>
2024-09-03 22:24:43 +0700

    Update

---
+++ src/app/Home/Home.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import './Home.css';
+const Home = () => {
+
+    return (
+        <div className="home-container">
+            Welcome to my project
+        </div>
+    );
+};
+
+export default Home;
